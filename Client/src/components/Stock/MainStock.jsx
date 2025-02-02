23334a2ee49f0d9ab85d35a84c979c26fdce2024
lucasn4 <168351521+lucasn4@users.noreply.github.com>
2025-02-02 16:47:05 -0300

    filtros-falta sucursal

--- Client/src/components/Stock/MainStock.jsx
+++ Client/src/components/Stock/MainStock.jsx
@@ -1,38 +1,154 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useStock } from "../../contexts/Stock/StockContext";
+import { format } from 'date-fns';
 
 const MainStock = () => {
   const { stock, loading, error, obtenerStockSucursal } = useStock();
-
+  const [stockFiltradoDefecto, setStockFiltradoDefecto] = useState();
+  const [stockFiltrado, setStockFiltrado] = useState();
+  const [busqueda, setBusqueda] = useState("");
+  const [lineaSeleccionada, setLineaSeleccionada] = useState("");
+  const [capacidadSeleccionada, setCapacidadSeleccionada] = useState("");
+  const [estadoStockSeleccionado, setEstadoStockSeleccionado] = useState("");
+  
   // Obtener el stock de la sucursal 1 al cargar el componente
   useEffect(() => {
-    obtenerStockSucursal(1); // Cambia el ID de la sucursal según sea necesario
+    obtenerStockSucursal(1);
+    limpiarFiltros(); // Limpiar filtros al cambiar de sucursal
   }, []);
 
+  // Filtrar los productos por fecha de última actualización (más reciente a más antigua)
+  useEffect(() => {
+    if (stock) {
+      const stockdefecto = [...stock].sort((a, b) => {
+        const dateA = new Date(a.ultima_actualizacion);
+        const dateB = new Date(b.ultima_actualizacion);
+        return dateB - dateA; // Ordenar de más reciente a más antiguo
+      });
+      setStockFiltradoDefecto(stockdefecto);
+    }
+    if (stockFiltradoDefecto) {
+      let filtrado = [...stockFiltradoDefecto];
+
+      if (busqueda) {
+        filtrado = filtrado.filter((item) =>
+          item.producto.toLowerCase().includes(busqueda.toLowerCase())
+        );
+      }
+
+      if (lineaSeleccionada) {
+        filtrado = filtrado.filter((item) => item.marca === lineaSeleccionada);
+      }
+
+      if (capacidadSeleccionada) {
+        filtrado = filtrado.filter(
+          (item) => item.capacidad === Number(capacidadSeleccionada)
+        );
+      }
+
+      if (estadoStockSeleccionado) {
+        filtrado = filtrado.filter(
+          (item) => item.estado_stock === estadoStockSeleccionado
+        );
+      }
+
+      setStockFiltrado(filtrado);
+    }
+  }, [stock,busqueda, lineaSeleccionada,capacidadSeleccionada, estadoStockSeleccionado]);
+  // Limpiar todos los filtros
+  const limpiarFiltros = () => {
+    setBusqueda("");
+    setLineaSeleccionada("");
+    setCapacidadSeleccionada("");
+    setEstadoStockSeleccionado("");
+};
+  const marcasDisponibles = [...new Set(stockFiltrado?.map(item => item.marca))];
+  const capacidadDisponibles = [...new Set(stockFiltrado?.map(item => item.capacidad))];
+  const estadoDisponibles = [...new Set(stockFiltrado?.map(item => item.estado_stock))];
+ 
   if (loading) return <p>Cargando stock...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
       <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Stock de la Sucursal</h1>
+      <div className="container-filtros" style={{ display: "flex", gap: "2%" }}>
+        
+        <input
+          type="text"
+          placeholder="Buscar por nombre..."
+          style={{ width: "10%", padding: "10px", marginBottom: "20px" }}
+          value={busqueda} // Vinculamos el estado de búsqueda al valor del input
+          onChange={(e) => setBusqueda(e.target.value)} // Actualizamos el estado al escribir
+        />
+        {/* Filtro por línea (marca) */}
+        <select
+          style={{ padding: "10px", marginBottom: "20px" }}
+          value={lineaSeleccionada}
+          onChange={(e) => setLineaSeleccionada(e.target.value)}
+        >
+          <option value="">Selecciona una Línea</option>
+          {marcasDisponibles.map((marca) => (
+            <option key={marca} value={marca}>
+              {marca}
+            </option>
+          ))}
+        </select>
+        {/* Filtro por capacidad */}
+        <select
+          style={{ padding: "10px", marginBottom: "20px" }}
+          value={capacidadSeleccionada}
+          onChange={(e) => setCapacidadSeleccionada(e.target.value)}
+        >
+        <option value="">Selecciona una Capacidad</option>
+          {capacidadDisponibles.map((capacidad) => {
+          const displayCapacidad = capacidad >= 1000 ? `${capacidad / 1000} L` : `${capacidad} ml`;
+          return (
+        <option key={capacidad} value={capacidad}>
+          {displayCapacidad}
+        </option>
+      );
+      })}
+      </select>
+      {/* Filtro por estado de stock */}
+      <select
+          style={{ padding: "10px", marginBottom: "20px" }}
+          value={estadoStockSeleccionado}
+          onChange={(e) => setEstadoStockSeleccionado(e.target.value)}
+        >
+          <option value="">Selecciona un Estado de Stock</option>
+          {estadoDisponibles.map((estado_stock) => (
+            <option key={estado_stock} value={estado_stock}>
+              {estado_stock}
+            </option>
+          ))}
+        </select>
+        <button onClick={limpiarFiltros} style={{ padding: "10px", marginBottom: "20px" }}>Limpiar Filtros</button>
+      </div>
       <table style={{ width: "100%", borderCollapse: "collapse" }}>
         <thead>
           <tr style={{ backgroundColor: "#f4f4f4" }}>
             <th style={{ padding: "10px", border: "1px solid #ddd" }}>Producto</th>
+            <th style={{ padding: "10px", border: "1px solid #ddd" }}>Linea</th>
+            <th style={{ padding: "10px", border: "1px solid #ddd" }}>Capacidad</th>
             <th style={{ padding: "10px", border: "1px solid #ddd" }}>Stock Mínimo</th>
             <th style={{ padding: "10px", border: "1px solid #ddd" }}>Estado del Stock</th>
             <th style={{ padding: "10px", border: "1px solid #ddd" }}>Stock Óptimo</th>
             <th style={{ padding: "10px", border: "1px solid #ddd", backgroundColor: "#e0f7fa" }}>Stock Total</th>
+            <th style={{ padding: "10px", border: "1px solid #ddd" }}>Ultima Actualización</th>
           </tr>
         </thead>
         <tbody>
-          {stock.map((item) => {
+          {(stockFiltrado || []).map((item) => {
             // Calcular el porcentaje de stock disponible respecto al stock óptimo
             const porcentajeStock = (item.cantidad_disponible / item.stock_optimo) * 100;
-
+            const porcentajeStockMinimo = ((item.stock_minimo* 100) / item.stock_optimo);
+            const capacidadfinal = item.capacidad>=1000 ? `${(item.capacidad/1000)}  L` : `${item.capacidad} ml`;
             return (
               <tr key={`${item.producto}-${item.sucursal}`} style={{ borderBottom: "1px solid #ddd" }}>
                 <td style={{ padding: "10px", border: "1px solid #ddd" }}>{item.producto}</td>
+                <td style={{ padding: "10px", border: "1px solid #ddd" }}>{item.marca}</td>
+                <td style={{ padding: "10px", border: "1px solid #ddd" }}>{capacidadfinal}</td>
                 <td style={{ padding: "10px", border: "1px solid #ddd", textAlign: "center" }}>{item.stock_minimo}</td>
                 <td style={{ padding: "10px", border: "1px solid #ddd" }}>
                   <div
@@ -62,14 +178,23 @@ const MainStock = () => {
                         left: 0,
                       }}
                     ></div>
+                    <div 
+                      style={{ position: "absolute",
+                       left: `${porcentajeStockMinimo}%`,
+                        top: 0,
+                         bottom: 0,
+                          width: "2px",
+                           background: "red" 
+                           }}
+                           ></div>
 
                     {/* Texto del estado y valor numérico */}
                     <div
                       style={{
                         position: "absolute",
                         top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
+                        left: "45%",
+                        transform: "translate(-45%, -50%)",
                         color: "#000",
                         fontWeight: "bold",
                         textAlign: "center",
@@ -83,6 +208,9 @@ const MainStock = () => {
                 <td style={{ padding: "10px", border: "1px solid #ddd", textAlign: "center" }}>{item.stock_optimo}</td>
                 <td style={{ padding: "10px", border: "1px solid #ddd", textAlign: "center", backgroundColor: "#e0f7fa", fontWeight: "bold" }}>
                   {item.cantidad_disponible}
+                </td>
+                <td style={{ padding: "10px", border: "1px solid #ddd", textAlign: "center" }}>
+                  {format(new Date(item.ultima_actualizacion), "dd-MM-yyyy - HH-mm-ss")}
                 </td>
               </tr>
             );
